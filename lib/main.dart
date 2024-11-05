01ea8db0ccbcdf67ee847132c7cec648f82624e7
Amr Aly <amraly6631@gmail.com>
2024-11-05 12:07:08 +0000

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'To-Do List',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(195, 6, 250, 67),
+          ),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const MyHomePage(),
+          '/tasks': (context) => const TasksPage()
+        });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('To-Do List'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "What Needs to Be Done?",
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 75.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 17.0),
+              child: TextField(
+                autofocus: false,
+                controller: _controller,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2.0,
+                    ),
+                  ),
+                  hintText: 'Enter a task',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  labelText: 'Task',
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  icon: const Icon(Icons.task),
+                  iconColor: Colors.green,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2.0,
+                    ),
+                  ),
+                  labelStyle: const TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              label: const Text(
+                'Add Task',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                  const Color.fromARGB(195, 6, 250, 67),
+                ),
+              ),
+            ),
+            Padding(
+              // you can use a stack and positioned to make the button go down more
+              padding: const EdgeInsets.only(top: 70.0, right: 20.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton.small(
+                  backgroundColor: const Color.fromARGB(195, 6, 250, 67),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Settings Menu'),
+                              IconButton(
+                                icon: const Icon(Icons.close),
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pop(); // Closes the dialog
+                                },
+                              ),
+                            ],
+                          ),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              ListTile(
+                                leading: const Icon(Icons.account_circle),
+                                title: const Text('Profile'),
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                  print("Profile selected");
+                                },
+                              ),
+                              ListTile(
+                                leading: const Icon(Icons.notifications),
+                                title: const Text('Notifications'),
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                  print("Notifications selected");
+                                },
+                              ),
+                              ListTile(
+                                leading: const Icon(Icons.settings),
+                                title: const Text('Settings'),
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                  print("Settings selected");
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: const Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 150, 255, 150),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Tasks',
+            )
+          ],
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+              if (index == 0) {
+                Navigator.pushNamed(context, '/');
+              } else if (index == 1) {
+                Navigator.pushNamed(context, '/tasks');
+              }
+            });
+          }),
+    );
+  }
+}
+
+class TasksPage extends StatefulWidget {
+  const TasksPage({super.key});
+
+  @override
+  State<TasksPage> createState() => _TasksPageState();
+}
+
+class _TasksPageState extends State<TasksPage> {
+  int _currentIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('To-Do List'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child:  Text('Tasks Page Under Construction',
+        style: TextStyle(
+          fontSize: 50.0,
+          fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,)
+        ),
+    bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 150, 255, 150),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Tasks',
+            )
+          ],
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+              if (index == 0) {
+                Navigator.pushNamed(context, '/');
+              } else if (index == 1) {
+                Navigator.pushNamed(context, '/tasks');
+              }
+            });
+          })
+    
+    );
+  }
+}
